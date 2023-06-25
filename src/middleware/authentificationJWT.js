4130eadf6c009f5964b105c4381510f2c24f77ba
Gibenewi <108399613+Gibewevi@users.noreply.github.com>
2023-06-25 14:46:34 +0200

    update JWT

---
+++ src/middleware/authentificationJWT.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+
+export const auth = async (req, res, next) => {
+
+    const token = req.cookies['jwt'];
+    if (!token) {
+        return res.redirect('/signin');
+    }
+
+    try {
+        const auth = jwt.verify(token, process.env.SECRET_KEY);
+        req.auth = auth;
+        next();
+    } catch (e) {
+        return res.redirect('/signin');
+    }
+};
