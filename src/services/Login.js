ca38faf4a37dd749bed7ca62eec9c11daff62edc
Gibenewi <108399613+Gibewevi@users.noreply.github.com>
2023-06-25 14:30:15 +0200

    update

--- src/services/Login.js
+++ src/services/Login.js
@@ -1,6 +1,6 @@
 
-// const API_URL = process.env.NEXT_PUBLIC_API_URL;
-const API_URL = "http://localhost:3000";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+// const API_URL = "http://localhost:3000";
 
 const signin = async (account) => {
 
